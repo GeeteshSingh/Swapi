4169cc90500d6f6a6626ef81cdae66889f073bf7
Geetesh Singh <geetesh@Geeteshs-MacBook-Pro.local>
2022-03-30 01:28:35 +0530

    bugs for scroll

--- src/species/InfiniteSpecies.jsx
+++ src/species/InfiniteSpecies.jsx
@@ -20,20 +20,21 @@ export function InfiniteSpecies() {
   } = useInfiniteQuery(
     "sw-species",
     ({ pageParam = initialUrl }) => {
-      fetchUrl(pageParam);
+      return fetchUrl(pageParam);
     },
-    { getNextPageParam: (lastPage) => lastPage.next || undefined }
+    {
+      getNextPageParam: (lastPage) => lastPage?.next || undefined,
+    }
   );
   if (isLoading) return <div className="loading">Loading...</div>;
   if (isError) return <div className="error">Error!! {error.toString()}</div>;
-  // TODO: get data for InfiniteScroll via React Query
 
   return (
     <>
       {isFetching && <div className="loading">Loading...</div>}
       <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
         {data.pages.map((pageData) => {
-          return pageData.results.map((species) => {
+          return pageData?.results?.map((species) => {
             return (
               <Species
                 key={species.name}
